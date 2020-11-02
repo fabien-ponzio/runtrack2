76c906a41a40460cc72254035dd19e0d363e0d17
fabien ponzio <fabien.ponzio@laplateforme.io>
2020-11-02 17:14:30 +0100

    Vfinale Job00 et job01

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,20 +1,44 @@
 <!DOCTYPE html>
+
 <html>
     <head>
         <title>Cours PHP & MySQL</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="cours.css">
     </head>
-    
-    <body>
-<?php 
 
-$str = "LaPlateforme_"
-$str2="Vive"
-$str3 = "!"
- echo $str2; echo$str, echo$str3;
+    <body>
+
+<?php
+$str = "LaPlateforme_";
+$str2="Vive";
+$str3 = "!";
+ echo $str2." ".$str." ".$str3;
+ echo "<br/>";
+
+ //FIN
+
+ $val = 6;
+ echo $val;
+ echo "<br/>";
+ $val +=4;
+ echo $val;
+ echo "<br/>";
+
+//FIN
+
+ $myBool = true;
+ echo $myBool;
+
+ echo "<br/>";
+$myBool = false; 
+echo $myBool;
+
+
+//FIN JOB01 
 ?>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, reprehenderit quis sint vero sequi ullam repudiandae eius harum impedit laborum maxime eveniet, ea aut odit beatae error atque maiores quo.</p>
+
+
 </body>
 </html>
+
