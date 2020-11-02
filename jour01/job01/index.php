beaeb0c05145b5379b561d3df1adcde54bf3f2d8
fabien ponzio <fabien.ponzio@laplateforme.io>
2020-11-02 15:55:30 +0100

    Update index.php

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,8 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP & MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    
+    <body>
 <?php 
 
-$str = echo"(LaPlateforme_)"
-$str2 = echo"(Vive)"
-$str3 = echo"(!)"
-
-echo("$str2") echo("str") echo("str3")
+$str = "LaPlateforme_"
+$str2="Vive"
+$str3 = "!"
+ echo $str2; echo$str, echo$str3;
 ?>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, reprehenderit quis sint vero sequi ullam repudiandae eius harum impedit laborum maxime eveniet, ea aut odit beatae error atque maiores quo.</p>
+</body>
+</html>
